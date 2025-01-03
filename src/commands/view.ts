3ef2dece1bd3b5d78fdde3047337f612ea84a457
nsonidotdev <anton.stecyura.pr1@gmail.com>
2025-01-03 13:20:18 +0200

    Implement todos viewing with blessed

--- src/commands/view.ts
+++ src/commands/view.ts
@@ -1,11 +1,12 @@
 import { Command } from "commander";
-import { validatePriority } from "../utils";
+import { stylePriority, validatePriority } from "../utils";
 import { TodoPriority } from "../enums";
 import { handleError } from "../lib";
 import { readFile } from "fs/promises";
 import { TODOS_PATH } from '../constants'
 import chalk from "chalk";
 import { Todo } from "../types";
+import blessed from 'blessed';
 
 type ViewOptions = {
     completed?: boolean;
@@ -58,11 +59,58 @@ viewTodosCommand.action(async (_, options) => {
         if (typeof commandOptions.incompleted === "boolean" && commandOptions.incompleted && todo.completed) {
             return false;
         }
-            
+
         return true
     })
 
-    console.log(filteredTodos)
+    if (filteredTodos.length === 0) {
+        console.log(chalk.yellow("No tasks match your filter criteria."));
+        return;
+    }
+
+    const screen = blessed.screen({
+        smartCSR: true,
+        title: "TODO List"
+    });
+
+    const todosBox = blessed.box({
+        parent: screen,
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        keys: true,
+        scrollable: true,
+        alwaysScroll: true,
+        scrollbar: {
+            ch: ' ',
+            style: { bg: 'red' },
+        },
+        style: {
+            fg: 'white',
+            bg: 'black',
+        },
+        content: filteredTodos.map((todo, index) => {
+            const status = todo.completed ? chalk.green("✔ Done") : chalk.red("✘ Pending");
+            return `${chalk.bold(`${index + 1}. ${todo.title}`)}\nStatus: ${status} Priority: ${stylePriority(todo.priority)}\n\n---------------------------------------------------------`;
+        }).join('\n\n'),
+    });
+
+    blessed.box({
+        parent: screen,
+        bottom: 0,
+        height: 1,
+        width: '100%',
+        style: {
+            fg: 'white',
+            bg: 'grey',
+        },
+        content: "Arrow Up/Down to scroll, q to quit",
+    });
+
+    todosBox.key(['q', 'C-c'], () => process.exit(0));
+
+    screen.render();
 })
 
 export { viewTodosCommand }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,23 @@
+import chalk from 'chalk';
+import { TodoPriority } from '../enums';
 
 
 export const sleep = (ms: number = 1000) => new Promise(r => setTimeout(r, ms));
 
+export const stylePriority = (priority: TodoPriority, displayText?: string): string => {
+    switch (priority) {
+        case TodoPriority.Low: 
+            return chalk.blueBright(displayText ?? priority)
+        case TodoPriority.Low: 
+            return chalk.yellow(displayText ?? priority)
+        case TodoPriority.Low: 
+            return chalk.redBright(displayText ?? priority)
+        case TodoPriority.Low: 
+            return chalk.red(displayText ?? priority);
+        default:
+            return priority;
 
+    }
+}
 
 export * from './validate';
