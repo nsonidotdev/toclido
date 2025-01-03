928a710adfe22d68c9be7329252fed1f13af8f12
nsonidotdev <anton.stecyura.pr1@gmail.com>
2025-01-03 14:00:56 +0200

    Fix priority styling

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,13 +6,13 @@ export const sleep = (ms: number = 1000) => new Promise(r => setTimeout(r, ms));
 
 export const stylePriority = (priority: TodoPriority, displayText?: string): string => {
     switch (priority) {
-        case TodoPriority.Low: 
+        case TodoPriority.Low:
             return chalk.blueBright(displayText ?? priority)
-        case TodoPriority.Low: 
+        case TodoPriority.Medium:
             return chalk.yellow(displayText ?? priority)
-        case TodoPriority.Low: 
+        case TodoPriority.High:
             return chalk.redBright(displayText ?? priority)
-        case TodoPriority.Low: 
+        case TodoPriority.Urgent:
             return chalk.red(displayText ?? priority);
         default:
             return priority;
